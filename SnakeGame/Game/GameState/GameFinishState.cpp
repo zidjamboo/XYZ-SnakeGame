7fba4572f787d55e9ac57fcb15f0be9a86fa4ebd
afadyukov <fadyukov.ak@gmail.com>
2026-01-04 17:31:11 +0300

    add a finish game popup draft

--- SnakeGame/Game/GameState/GameFinishState.cpp
+++ SnakeGame/Game/GameState/GameFinishState.cpp
@@ -1,5 +1,14 @@
 ﻿#include "GameFinishState.h"
 
+#include "../../Constants.h"
+
+namespace
+{
+    constexpr float POPUP_X_SIZE = 300.f;
+    constexpr float POPUP_Y_SIZE = 500.f;
+    constexpr float BORDER_SIZE = 10.f;
+}
+
 void SnakeGame::GameFinishState::onActive()
 {
 }
@@ -10,12 +19,21 @@ void SnakeGame::GameFinishState::onUpdate(Game& game, const float& deltaTime)
 
 void SnakeGame::GameFinishState::onDraw(Game& game, sf::RenderWindow& window)
 {
-    sf::RectangleShape rect;
-    rect.setSize({50.f, 50.f});
-    rect.setPosition({0.f, 0.f});
-    rect.setFillColor(sf::Color::White);
+    sf::RectangleShape border;
+    border.setSize({POPUP_X_SIZE, POPUP_Y_SIZE});
+    sf::Vector2f whiteBackgroundPosition = {(SCREEN_WIDTH - POPUP_X_SIZE) / 2.f, (SCREEN_HEIGHT - POPUP_Y_SIZE) / 2.f};
+    border.setPosition(whiteBackgroundPosition);
+    border.setFillColor(sf::Color::White);
 
-    window.draw(rect);
+    sf::RectangleShape innerRect;
+    innerRect.setSize({POPUP_X_SIZE - BORDER_SIZE * 2, POPUP_Y_SIZE - BORDER_SIZE * 2});
+    innerRect.setPosition({whiteBackgroundPosition.x + BORDER_SIZE, whiteBackgroundPosition.y + BORDER_SIZE});
+    innerRect.setFillColor(sf::Color::Black);
+
+
+
+    window.draw(border);
+    window.draw(innerRect);
 }
 
 void SnakeGame::GameFinishState::onInactive()
